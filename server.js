90d305b029169bbd3069d4df718fb63c3117f58a
Sooriya N <nagarajansooriya@gmail.com>
2025-10-23 22:15:20 +0530

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://yaalir-travel-planning-app.vercel.app'
+    process.env.FRONTEND_URL || 'https://yaalir-travel-planning-app.vercel.app'
   ],
   credentials: true,
   optionsSuccessStatus: 200
